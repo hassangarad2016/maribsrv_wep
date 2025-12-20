a39ef749d81c095ec75f73616070b8f05aba5326
maribservices <maribservices@gmail.com>
2025-12-21 00:36:01 +0300

    msg

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,16 +3,25 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class TrustProxies extends Middleware
 {
     protected $proxies = '*';
 
-    // بديل آمن عن HEADER_X_FORWARDED_ALL (غير موجود عندك)
-    protected $headers =
-        SymfonyRequest::HEADER_X_FORWARDED_FOR |
-        SymfonyRequest::HEADER_X_FORWARDED_HOST |
-        SymfonyRequest::HEADER_X_FORWARDED_PORT |
-        SymfonyRequest::HEADER_X_FORWARDED_PROTO;
+    protected $headers;
+
+    public function __construct()
+    {
+        // استخدم AWS_ELB إن وجد، وإلا فالأعلام الأساسية فقط.
+        $this->headers = \defined(Request::class . '::HEADER_X_FORWARDED_AWS_ELB')
+            ? Request::HEADER_X_FORWARDED_AWS_ELB
+            : (
+                SymfonyRequest::HEADER_X_FORWARDED_FOR |
+                SymfonyRequest::HEADER_X_FORWARDED_HOST |
+                SymfonyRequest::HEADER_X_FORWARDED_PORT |
+                SymfonyRequest::HEADER_X_FORWARDED_PROTO
+            );
+    }
 }
