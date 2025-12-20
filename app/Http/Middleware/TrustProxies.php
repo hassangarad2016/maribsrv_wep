950cb81c21c923c21709286ca1fe0eb80950b03e
maribservices <maribservices@gmail.com>
2025-12-21 00:24:04 +0300

    msg

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,7 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
-use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class TrustProxies extends Middleware
 {
@@ -19,6 +19,5 @@ class TrustProxies extends Middleware
      *
      * @var int
      */
-    // Use AWS_ELB constant for broader compatibility on older Laravel versions.
-    protected $headers = Request::HEADER_X_FORWARDED_AWS_ELB;
+    protected $headers = SymfonyRequest::HEADER_X_FORWARDED_ALL;
 }
